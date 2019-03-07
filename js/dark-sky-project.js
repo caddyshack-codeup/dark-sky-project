8dd71d0686d9d8f3f74ee5fc391ac257a4e5ba71
jerlogan <jeremy.logan.web@gmail.com>
2019-03-07 11:53:08 -0600

    converted API data to html string

--- js/dark-sky-project.js
+++ js/dark-sky-project.js
@@ -3,16 +3,44 @@ $(document).ready(function() {
 
     var darkSkyUrl = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/29.4241,-98.4936";
 
-    $.get(darkSkyUrl).done(function(data) {
-        console.log(data);
-    });
 
 
-    mapboxgl.accessToken = mapboxAccessToken;
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
-    });
+        $.get(darkSkyUrl).done(function (data) {
+            var temperature = data.currently.temperature;
+            var feelsLike = data.currently.apparentTemperature;
+            var icon = data.currently.icon;
+            var summary = data.currently.summary;
+            var humidity = data.currently.humidity;
+            var string = '';
+            string += "<p id='temperature'>" +  '<h2>' + Math.round(temperature) + '&#176' + '</h2>';
+            string += "<div id='icon'>" + '</div>';
+            string += "<p id='summary'>" + summary + '</p>';
+            string += "<p id='humidity'>" + 'Humidity: ' + (humidity * 100) + '%' + '</p>';
+
+            $('.left-box').html(string);
+
+            //     <h2>53</h2>
+            //     <p>feels like</p>
+            // <img src="Icon/Cloud-Drizzle-Moon-Alt.svg">
+            //     <p>summary</p>
+            //     <p>humidity</p>
+
+        });
+
+
+
+
+
+    function mapBox() {
+        mapboxgl.accessToken = mapboxAccessToken;
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9'
+        });
+    }
+
+   mapBox();
+
 
 
 
---
+++ js/keys.js
@@ -0,0 +1,3 @@
+var darkSkyToken = "f9f09e56a7ba38aadf5ecc54f3b8e269";
+
+var mapboxAccessToken = "pk.eyJ1IjoiamVybG9nYW4iLCJhIjoiY2pza21nNzh1Mnl6cDN5b2E2cWczYXpncCJ9.U5POBulBL3zmcahwTxC2Qw";
